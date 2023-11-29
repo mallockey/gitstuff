44e687a563377e751330c579a1a22cf351d67e7b
Joshua Melo <joshuamelo@gmail.com>
2023-11-28 23:42:06 -0500

    big-featrue

--- index.ts
+++ index.ts
@@ -10,3 +10,12 @@ console.log('This is changes that have happened on main before you feature');
 console.log('I need to do more things in my new feature');
 
 console.log('This should be fun');
+
+
+console.log("I'm adding my big feature")
+
+const arr = [1,2,3]
+
+arr.forEach(item => item)
+
+console.log("okay this is the big feature")
